7fa440418839e432a262ea46ce1d92d74032df0d
zuoxiaolei <1905561110@qq.com.com>
2023-04-26 23:36:21 +0800

    auto commit

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ interpreter=/home/zxl/anaconda3/bin/python
 alias python3=${interpreter}
 export PYSPARK_PYTHON=${interpreter}
 export PYSPARK_DRIVER_PYTHON=${interpreter}
-${interpreter} qtrade.py >nohup.out 2>&1
+${interpreter} qtrade.py >qtrade.out 2>&1
 git add *
 git commit -m "auto commit"
-git push origin master
+git push origin main
