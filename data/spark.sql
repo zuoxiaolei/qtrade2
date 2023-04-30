9f8c19e386000f367c7a1c2218eeef99d5cdb9e7
zuoxiaolei <1905561110@qq.com>
2023-04-30 12:47:35 +0800

    fix qtrade.py

--- data/spark.sql
+++ data/spark.sql
@@ -54,7 +54,6 @@ from (
                   on t1.code=t2.code
                 left join fund_etf_fund_daily_em_df t3
                        on t1.code=t3.code
-                where t2.scale>=10
                  ) t
              )t
         where rate_1<0 and rate_2<0
@@ -66,3 +65,90 @@ where success_cnt/fund_cnt>=0.6
 order by date, code
 ;
 
+select
+           code,
+           name,
+           date,
+           scale,
+           profit,
+           success_cnt/fund_cnt success_rate,
+           rate_1,
+           rate_2,
+           rate_3,
+            rate_4,
+            rate_5,
+            rate_6,
+            code_cnt,
+            rate_1+rate_2+rate_3+rate_4+rate_5 rate_sum
+from (
+    select
+           code,
+           name,
+           date,
+           scale,
+           profit,
+           count(1) over(partition by code order by date) fund_cnt,
+           count(if(profit>0,code,null)) over(partition by code order by date) success_cnt,
+            rate_1,
+            rate_2,
+            rate_3,
+            rate_4,
+            rate_5,
+            rate_6,
+            count(code) over(partition by date) code_cnt
+    from (
+        select
+            code,
+            name,
+            date,
+            scale,
+            if(ahead_rate_1>0, ahead_rate_1, ahead_rate_2) profit,
+            rate_1,
+            rate_2,
+            rate_3,
+            rate_4,
+            rate_5,
+            rate_6
+        from (
+            select
+                code,
+                name,
+                date,
+                scale,
+                (close-close_1)/close_1 rate_1,
+                (close_1-close_2)/close_2 rate_2,
+                (close_2-close_3)/close_3 rate_3,
+                (close_3-close_4)/close_4 rate_4,
+                (close_4-close_5)/close_5 rate_5,
+                (close_5-close_6)/close_6 rate_6,
+                (ahead_1-close)/close ahead_rate_1,
+                (ahead_2-close)/close ahead_rate_2
+            from (
+                select t1.code,
+                       t3.name,
+                       date,
+                       close,
+                       t2.scale,
+                       lag(close, 1) over (partition by t1.code order by date) close_1,
+                       lag(close, 2) over (partition by t1.code order by date) close_2,
+                       lag(close, 3) over (partition by t1.code order by date) close_3,
+                       lag(close, 4) over (partition by t1.code order by date) close_4,
+                       lag(close, 5) over (partition by t1.code order by date) close_5,
+                       lag(close, 6) over (partition by t1.code order by date) close_6,
+                       lead(close, 1) over (partition by t1.code order by date) ahead_1,
+                       lead(close, 2) over (partition by t1.code order by date) ahead_2
+                from df t1
+                join scale_df t2
+                  on t1.code=t2.code
+                left join fund_etf_fund_daily_em_df t3
+                       on t1.code=t3.code
+                 ) t
+             )t
+        where rate_1<0 and rate_2<0
+              and rate_3<3 and rate_4<0
+              and rate_5<0 and rate_6>0
+         ) t
+     ) t
+where success_cnt/fund_cnt>=0.6
+order by date, code
+;
--- qtrade.py
+++ qtrade.py
@@ -1,9 +1,14 @@
+import random
 from datetime import datetime
+
+import joblib
+import lightgbm
 import psutil
 import pytz
 import time
 import pandas as pd
 from pyspark.sql import SparkSession
+from sklearn.metrics import classification_report
 
 cpu_count = psutil.cpu_count()
 windows = 110
@@ -39,6 +44,60 @@ def get_spark():
     return spark
 
 
+def process_data(is_local=False):
+    stock_df_filename = qdata_prefix + "ads/exchang_fund_rt.csv"
+    scale_df_filename = qdata_prefix + "dim/scale.csv"
+    fund_etf_fund_daily_em_df_filename = qdata_prefix + "dim/exchang_eft_basic_info.csv"
+    stock_cnt_filename = qdata_prefix + "ads/stock_cnt_rt.csv"
+    if is_local:
+        stock_df_filename = "data/ads/exchang_fund_rt.csv"
+        scale_df_filename = "data/dim/scale.csv"
+        fund_etf_fund_daily_em_df_filename = "data/dim/exchang_eft_basic_info.csv"
+        stock_cnt_filename = "data/ads/stock_cnt_rt.csv"
+    stock_df = pd.read_csv(stock_df_filename, dtype={"code": object})
+    scale_df = pd.read_csv(scale_df_filename, dtype={"code": object})
+    fund_etf_fund_daily_em_df = pd.read_csv(fund_etf_fund_daily_em_df_filename, dtype={'基金代码': object})
+    fund_etf_fund_daily_em_df = fund_etf_fund_daily_em_df[['基金代码', '基金简称']]
+    fund_etf_fund_daily_em_df.columns = ['code', 'name']
+    stock_cnt = pd.read_csv(stock_cnt_filename, dtype={"code": object})
+
+    spark = get_spark()
+    df = spark.createDataFrame(stock_df)
+    scale_df = spark.createDataFrame(scale_df)
+    fund_etf_fund_daily_em_df = spark.createDataFrame(fund_etf_fund_daily_em_df)
+    df.createOrReplaceTempView("df")
+    scale_df.createOrReplaceTempView("scale_df")
+    fund_etf_fund_daily_em_df.createOrReplaceTempView("fund_etf_fund_daily_em_df")
+    result = spark.sql(spark_sql[1])
+    stock_cnt["increase_rate"] = stock_cnt["increase_cnt"] / stock_cnt["decrease_cnt"]
+    result_df = result.toPandas()
+    result_df = result_df.merge(stock_cnt, on=['date'])
+    return result_df
+
+
+def train_lightgbm(is_local=False):
+    result_df = process_data(is_local=is_local)
+    result_df.index = range(len(result_df))
+    index = list(range(len(result_df)))
+    random.shuffle(index)
+    num = int(0.7 * len(index))
+    train_index = index[:num]
+    test_index = index[num:]
+    featurs_col = ['scale', 'success_rate', 'rate_1',
+                   'rate_2', 'rate_3', 'rate_4', 'rate_5', 'rate_6', 'code_cnt', 'rate_sum', 'increase_cnt',
+                   'decrease_cnt', 'increase_rate']
+    result_df["label"] = result_df['profit'].map(lambda x: 1 if x > 0 else 0)
+    train_df, test_df = result_df.loc[train_index], result_df.loc[test_index]
+    model = lightgbm.LGBMClassifier(num_leaves=1000,
+                                    n_estimators=100000)
+    model.fit(train_df[featurs_col], train_df["label"],
+              eval_set=[(train_df[featurs_col], train_df["label"]), (test_df[featurs_col], test_df["label"])],
+              early_stopping_rounds=50)
+    y_pred = model.predict(test_df[featurs_col])
+    print(classification_report(test_df["label"], y_pred))
+    joblib.dump(model, "data/stock_lightgbm.model")
+
+
 def write_table(title, columns, df):
     string = ""
     string += title + "\n"
@@ -53,33 +112,16 @@ def write_table(title, columns, df):
 
 
 def run_down_buy_strategy(is_local=False):
-    stock_df_filename = qdata_prefix + "ads/exchang_fund_rt.csv"
-    scale_df_filename = qdata_prefix + "dim/scale.csv"
-    fund_etf_fund_daily_em_df_filename = qdata_prefix + "dim/exchang_eft_basic_info.csv"
-    if is_local:
-        stock_df_filename = "data/ads/exchang_fund_rt.csv"
-        scale_df_filename = "data/dim/scale.csv"
-        fund_etf_fund_daily_em_df_filename = "data/dim/exchang_eft_basic_info.csv"
-
-    stock_df = pd.read_csv(stock_df_filename, dtype={"code": object})
-    scale_df = pd.read_csv(scale_df_filename, dtype={"code": object})
-    fund_etf_fund_daily_em_df = pd.read_csv(fund_etf_fund_daily_em_df_filename, dtype={'基金代码': object})
-    fund_etf_fund_daily_em_df = fund_etf_fund_daily_em_df[['基金代码', '基金简称']]
-    fund_etf_fund_daily_em_df.columns = ['code', 'name']
-
-    spark = get_spark()
-    df = spark.createDataFrame(stock_df)
-    scale_df = spark.createDataFrame(scale_df)
-    fund_etf_fund_daily_em_df = spark.createDataFrame(fund_etf_fund_daily_em_df)
-    df.createOrReplaceTempView("df")
-    scale_df.createOrReplaceTempView("scale_df")
-    fund_etf_fund_daily_em_df.createOrReplaceTempView("fund_etf_fund_daily_em_df")
-    result = spark.sql(spark_sql[0])
-    buy_stocks = result.toPandas()
+    buy_stocks = process_data(is_local=is_local)
     buy_stocks = buy_stocks.sort_values(["code", "date"])
+    model = joblib.load("data/stock_lightgbm.model")
+    featurs_col = ['scale', 'success_rate', 'rate_1',
+                   'rate_2', 'rate_3', 'rate_4', 'rate_5', 'rate_6', 'code_cnt', 'rate_sum', 'increase_cnt',
+                   'decrease_cnt', 'increase_rate']
+    buy_stocks["pred"] = model.predict(buy_stocks[featurs_col])
+    buy_stocks = buy_stocks["code,name,date,scale,profit,success_rate,pred".split(",")]
     buy_stocks.to_csv("data/ads/history_data.csv", index=False)
     buy_stocks = buy_stocks[buy_stocks.date == buy_stocks.date.max()]
-    spark.stop()
 
     string = ""
     if len(buy_stocks):
